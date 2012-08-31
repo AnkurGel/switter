7743e48bd89363c8c9660a602d0b6abcd25babd6
Ankur Goel <ankur@ankur-vostro.(none)>
2012-08-31 18:53:10 +0530

    Generated rails structure and laid bare backbones. Added debugger and few aiding gems for console tasks. Added pg for heroku.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Switter::Application.config.secret_token = 'f178db04546d81b44c1b0c316ae8efefa022e9b9613f45389e88cd6b33a04122c90d503cd14904db4f2a359fe7251af3624ee50c89f6d1c424067d2e80fe4c3d'
