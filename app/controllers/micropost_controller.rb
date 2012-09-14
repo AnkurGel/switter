f05e431ef97302e6bfcda83f9db00b8aba12804c
Ankur Goel <ankurgel@gmail.com>
2012-09-14 20:49:26 +0530

    Fixed production error

--- app/controllers/micropost_controller.rb
+++ app/controllers/micropost_controller.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationController
-  before_filter :signed_in_user :only => [:create, :destroy]
+  before_filter :signed_in_user, :only => [:create, :destroy]
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
